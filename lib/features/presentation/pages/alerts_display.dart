4a15899ac48dcd450b487798062da621b6092337
Joseildo <filhojoseildo@gmail.com>
2021-01-02 19:27:29 -0300

    improving UI

--- lib/features/presentation/pages/alerts_display.dart
+++ lib/features/presentation/pages/alerts_display.dart
@@ -1,15 +1,19 @@
 import 'package:AligatorMobile/features/domain/entities/alert.dart';
 import 'package:AligatorMobile/features/presentation/controllers/alerts_display_controller.dart';
+import 'package:AligatorMobile/features/presentation/controllers/alerts_state.dart';
 import 'package:AligatorMobile/features/presentation/widgets/alert_display.dart';
 import 'package:AligatorMobile/features/presentation/widgets/circular_loading.dart';
 import 'package:AligatorMobile/features/presentation/widgets/main_title.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class AlertsDisplay extends StatelessWidget {
   final AlertsDisplayController _alertDisplayController;
 
-  AlertsDisplay(this._alertDisplayController) {}
+  AlertsDisplay(this._alertDisplayController) {
+    this._alertDisplayController.fetchData();
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -27,12 +31,19 @@ class AlertsDisplay extends StatelessWidget {
         ],
       );
 
-  Widget alerts() => CircularLoading(
-        future: this._alertDisplayController.fetchData(),
-        whenComplete: (_, __) => ListView(
-          children:
-              this._alertDisplayController.alerts.map(_buildAlert).toList(),
-        ),
+  Widget alerts() => Observer(
+        builder: (BuildContext context) {
+          final state = this._alertDisplayController.alertsState;
+          if (state is Loaded)
+            return ListView(
+              children: state.alerts.map(_buildAlert).toList(),
+            );
+          else if (state is Loading) {
+            return CircularLoading();
+          } else if (state is Error) {
+            return Container();
+          }
+        },
       );
 
   Widget _buildAlert(Alert alert) => AlertDisplay(alert);
--- lib/features/presentation/widgets/circular_loading.dart
+++ lib/features/presentation/widgets/circular_loading.dart
@@ -1,38 +1,13 @@
 import 'package:flutter/material.dart';
 
-typedef Widget WhenComplete(BuildContext context, dynamic value);
-
-class CircularLoading<T> extends StatefulWidget {
-  final Future<T> _future;
-  final WhenComplete _whenComplete;
-
-  CircularLoading({
-    @required Future future,
-    @required WhenComplete whenComplete,
-  })  : _future = future,
-        _whenComplete = whenComplete;
-
+class CircularLoading extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() => _CircularLoading<T>();
-}
-
-class _CircularLoading<T> extends State<CircularLoading> {
-  bool _isLoading = true;
-  T _returnedValue;
-
-  _CircularLoading() {
-      print('Future');
-      print(widget._future);
-    widget._future.then((value) {
-      setState(() {
-        this._isLoading = false;
-        this._returnedValue = value;
-      });
-    });
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 3,
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
-
-  @override
-  Widget build(BuildContext context) => this._isLoading
-      ? CircularProgressIndicator()
-      : widget._whenComplete(context, this._returnedValue);
 }
